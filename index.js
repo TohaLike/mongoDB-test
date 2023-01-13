f3b928fdb8a09115bd030a7a1cc4f5f480015db6
Anton <thetoxalike444@gmail.com>
2023-01-13 03:28:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routers/todos');
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main', 
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(todoRoutes);
+
+// mongodb+srv://<username>:<password>@cluster0.q9ecejt.mongodb.net/test
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://AntonM:554431@cluster0.q9ecejt.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+
+        app.listen(PORT, () => {
+            console.log('Server has been started...');
+        });
+
+    } catch (e) {
+        console.log(e)
+    };
+};
+
+start();
+
+// console.log('Hello world')
