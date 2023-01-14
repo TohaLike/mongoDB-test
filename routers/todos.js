72e442a4ab07ebcc653a5eef2e3761feab85f038
Anton Malyshev <90730699+TohaLike@users.noreply.github.com>
2023-01-14 03:30:43 +0300

    i add new function for pages

--- routers/todos.js
+++ routers/todos.js
@@ -1,8 +1,22 @@
 const {Router} = require('express');
+const todo = require('../models/Todo')
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    
+
+    res.render('index', {
+        title: 'Todos List', 
+        isIndex: true
+    });
+});
+
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create Todo',
+        isCreate: true
+    });
 });
 
 module.exports = router
